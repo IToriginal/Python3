8dd094331106ed2dff926874030be93d68b96c50
IToriginal <gitshineit@gmail.com>
2023-03-07 23:56:54 +0900

    fix: KCSdev2/1/2/행렬의 곱 - NumPy 라이브러리

--- KSCDev2nd/행렬의 곱.py
+++ KSCDev2nd/행렬의 곱.py
@@ -1,7 +1,16 @@
+# NumPy 라이브러리 사용
+import numpy as np #NumPy 라이브러리 사용
+
 def solution(arr1, arr2):
-    answer = [[0]*len(arr2[0]) for _ in range(len(arr1))]
-    for i in range(len(arr1)):
-        for j in range(len(arr2[0])): 
-            for k in range(len(arr1[0])): 
-                answer[i][j] += arr1[i][k] * arr2[k][j]
-    return answer
+		# arr1과 arr2를 NumPy 행렬로 변환한다.
+    arr1 = np.array(arr1)
+    arr2 = np.array(arr2)
+
+		#행렬의 곱셉을 수행하여 결과를 반환한다.
+    result = np.dot(arr1, arr2)
+
+		#NumPy 배열을 리스트로 변환하여 반환한다.
+    return result.tolist()
+
+print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
